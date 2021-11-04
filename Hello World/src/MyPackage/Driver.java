95347d9c996dd8486b9057a32d3b498fe71de532
Ben <benjamin.aviv@revature.net>
2021-11-04 11:09:15 -0400

    first commit

---
+++ Hello World/src/MyPackage/Driver.java
@@ -0,0 +1,14 @@
+package MyPackage;
+
+public class Driver {
+	
+	public static void main(String[] args){
+		System.out.print("Hello world");
+
+		// Object INSTANTIATION:
+		MyThing mt = new MyThing();
+	
+		System.out.println("Say hit to my object, it has a value of " + mt.mySmallNumber);
+	}
+}
+	
---
+++ Hello World/src/MyPackage/MyThing.java
@@ -0,0 +1,9 @@
+package MyPackage;
+
+public class MyThing {
+	int myWholeNumber; // Short for 'integer'. 32 bits . Whole numbers only, eg 1, 5
+	float myDecimal; // 'Floating-point' or decimal number. 64 bits. Notoriously inaccurate
+
+	short mySmallNumber =5;
+}
+
